0d974105189711f0edcb9e09c6b1c536e9651cc0
Timurkaaa <119705363+Timurkaaaaaaa@users.noreply.github.com>
2025-01-05 12:35:29 +0300

    🟢 Initial commit

---
+++ ProgressBar/ProgressBar.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def ProgressBar(value, fullBar):
+    print("[ ", end="")
+    for i in range(0, fullBar+1):
+        if i <= value:
+            print("#", end="")
+        else:
+            print("-", end="")
+    print(" ]", end="")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ProgressBar',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Укажите ваши зависимости здесь
+    ],
+    description='Описание вашего модуля',
+    author='Timurkaaa',
+    author_email='ваш_email@example.com',
+    url='https://github.com/ваш_репозиторий',
+)
